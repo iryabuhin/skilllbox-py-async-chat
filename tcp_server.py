aa67bd9829e9027e76c9c9755fb6d58ec57bc5fc
narcissusashes <narcissusashes@pop-os.localdomain>
2020-04-08 17:40:01 +0300

    Initial commit

---
+++ tcp_server.py
@@ -0,0 +1,98 @@
+import asyncio
+from asyncio import transports
+from typing import Optional
+
+
+class ServerProtocol(asyncio.Protocol):
+    login: str = None
+    server: 'Server'
+    transport: transports.Transport
+
+    def __init__(self, server: 'Server'):
+        self.server = server
+
+    # base protocol overrides
+    def data_received(self, data: bytes):
+        content = data.decode()
+        print(content)
+
+        if self.login is not None:
+            self.send_message(content)
+            self.server.msg_history.append(content)
+        else:
+            if content.startswith('login'):
+                login = content.replace('login:', '').rstrip()
+
+                for user in self.server.clients:
+                    if user.login == login:
+                        self.transport.write('This login is already taken, try another one!'.encode())
+                        return
+
+                self.login = login
+                self.server.clients.append(self)
+                self.server.send_history(self)
+            else:
+                self.transport.write('Invalid login message!\n'.encode())
+
+    def connection_lost(self, exception):
+        self.server.clients.remove(self)
+
+    def connection_made(self, transport: transports.Transport):
+        self.server.clients.append(self)
+        self.transport = transport
+        print('New connection established')
+
+
+    def send_message(self, content: str):
+        msg = f'{self.login}: {content.rstrip()}\n'
+
+        for user in self.server.clients:
+            print(f'sent message to {user.login} from {self.login}')
+            user.transport.write(msg.encode())
+
+
+class Server:
+    clients: list
+    msg_history: list
+    protocol = ServerProtocol
+
+    def __init__(self):
+        self.clients = []
+        self.msg_history = []
+
+    def build_protocol(self):
+        return ServerProtocol(self)
+
+    def send_history(self, client: ServerProtocol):
+        last_messages = self.msg_history[-10:]
+
+        client.transport.write(f'Welcome, {client.login}!\n'.encode())
+
+        for message in last_messages:
+            client.transport.write(message.encode())
+
+
+    async def start(self):
+        loop = asyncio.get_running_loop()
+
+        subroutine = await loop.create_server(
+            protocol_factory=self.build_protocol,
+            host='127.0.0.1',
+            port=12234
+        )
+        print('Server started...')
+
+        await subroutine.serve_forever()
+
+
+def main():
+
+    process = Server()
+
+    try:
+        asyncio.run(process.start())
+    except KeyboardInterrupt:
+        print('\nServer stopped manually (keyboard interrupted)')
+
+if __name__ == '__main__':
+    main()
